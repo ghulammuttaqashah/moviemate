6645127fda0a91e13179d3c1154ac71bac00d871
Ghulam Muttaqa Shah <ghulammuttaqashah@gmail.com>
2025-08-30 11:38:29 +0500

    toast timing updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
    useEffect(() => {
     toast(
       "🍪 To login, please allow third-party cookies in your browser.",
-      { duration: 8000 }
+      { duration: 5000 }
     );
   }, []);
   return (
